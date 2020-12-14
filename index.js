025d7d5460071ef379f4c2e107bac1aef86ecab7
Mark Edward Ratio <markedwardratio@gmail.com>
2020-12-14 12:15:24 +0800

    Add node.js server file

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 const http = require('http');
-const url = require('url');
-const fs = require('fs');
 
 http
 	.createServer((req, res) => {
-		const q = url.parse(req.url, true);
-		const filePath = `./public${q.pathname}`;
-		fs.readFile(filePath, (err, data) => {
-			if (err) {
-				res.writeHead(400, { 'Content-Type': 'text/html' });
-				return res.end();
-			} else {
-				res.writeHead(200, { 'Content-Type': 'text/html' });
-				res.write(data);
-				return res.end();
-			}
-		});
+		res.write('Hello World');
+		res.end();
 	})
 	.listen(8080);
