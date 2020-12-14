cd971f2278c463df24020e9fdf2a3c313a72b94d
Mark Edward Ratio <markedwardratio@gmail.com>
2020-12-14 12:16:54 +0800

    Add the code needed to serve the right page according to the url

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 http
 	.createServer((req, res) => {
-		res.write('Hello World');
-		res.end();
+		const q = url.parse(req.url, true);
+		let filePath = '';
+
+		if (q.pathname === '/') {
+			filePath = `./public/index.html`;
+		} else {
+			filePath = `./public${q.pathname}`;
+		}
+
+		fs.readFile(filePath, (err, data) => {
+			if (err) {
+				res.writeHead(400, { 'Content-Type': 'text/html' });
+				fs.readFile('./public/404.html', (err, data) => {
+					if (err) {
+						throw err;
+					} else {
+						res.write(data);
+						return res.end();
+					}
+				});
+			} else {
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write(data);
+				return res.end();
+			}
+		});
 	})
 	.listen(8080);
